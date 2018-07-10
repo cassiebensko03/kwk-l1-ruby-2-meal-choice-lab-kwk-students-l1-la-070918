05de7686ce9735a1db6deb8ff62661d36edaa460
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-10 20:27:03 +0000

    automatically backed up by learn

---
+++ food.rb
@@ -0,0 +1,14 @@
+def breakfast(food="frosted flakes")
+  puts "Breakfast is the very best time for #{food}."
+end
+breakfast("toast")
+
+def lunch(food="grilled cheese")
+  puts "Lunch is the very best time for #{food}."
+end
+lunch("bread")
+
+def dinner(food="salmon")
+  puts "Dinner is the very best time for #{food}."
+end
+dinner("kool-aid")
